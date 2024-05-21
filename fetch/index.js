240d5f990680f8ff8e5b7aa80008a159aa31fcf1
Christian Abreu <christianabreuh@gmail.com>
2024-05-21 11:12:33 -0400

    Sandbox (#1)
    * Adding vercel url

--- fetch/index.js
+++ fetch/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const baseURL = process.env.VERCEL_URL || "";
 export const sendNotes = (body) => {
-  return axios.post("/api/", body);
+  return axios.post(`${baseURL}/api`, body);
 };
