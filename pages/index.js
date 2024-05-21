794893dbf26ae4624fcb8821a78cc59b1548895b
Christian64 <christianabreuh@gmail.com>
2024-05-21 11:45:47 -0400

    Fix: Title of website.
    Change the title of website to "AI Flashcards"

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { useState, useRef } from "react";
 import { sendNotes } from "@/fetch";
 import { Cards } from "@/components/Cards/Cards";
@@ -24,44 +26,50 @@ export default function Home() {
   });
 
   return (
-    <main>
-      <Header />
-      <div className="flex flex-col items-center m-auto max-w-[800px] gap-3 mt-10">
-        <header className="flex flex-col items-center text-center gap-2">
-          <h1
-            className={`${dmSerifDisplay.className} text-[3rem] font-bold  leading-10 w-[500px]`}
-          >
-            Create flashcards with AI.
-          </h1>
-          <p className={`${roboto.className} text-lg`}>
-            Paste/type your notes below and see the magic 🪄.
-          </p>
-        </header>
+    <>
+      <Head>
+        <title>AI Flashcards - Turn your notes in flashcards.</title>
+      </Head>
 
-        <div>
-          <textarea
-            ref={textareaRef}
-            className={`${roboto.className} textarea bg-white text-black textarea-md w-[500px] m-auto resize-none`}
-            placeholder="Software engineering is the branch of computer science that deals with the design, development, testing, and maintenance of software applications."
-            rows={7}
-          ></textarea>
-          <button
-            className={`${roboto.className} btn btn-primary flex m-auto px-[4rem] shadow-md mt-1`}
-            onClick={() => {
-              mutation.mutate({
-                notes: textareaRef.current.value,
-              });
-            }}
-          >
-            <MagicIcon />
-            Generate
-          </button>
+      <main>
+        <Header />
+        <div className="flex flex-col items-center m-auto max-w-[800px] gap-3 mt-10">
+          <header className="flex flex-col items-center text-center gap-2">
+            <h1
+              className={`${dmSerifDisplay.className} text-[3rem] font-bold  leading-10 w-[500px]`}
+            >
+              Create flashcards with AI.
+            </h1>
+            <p className={`${roboto.className} text-lg`}>
+              Paste/type your notes below and see the magic 🪄.
+            </p>
+          </header>
+
+          <div>
+            <textarea
+              ref={textareaRef}
+              className={`${roboto.className} textarea bg-white text-black textarea-md w-[500px] m-auto resize-none`}
+              placeholder="Software engineering is the branch of computer science that deals with the design, development, testing, and maintenance of software applications."
+              rows={7}
+            ></textarea>
+            <button
+              className={`${roboto.className} btn btn-primary flex m-auto px-[4rem] shadow-md mt-1`}
+              onClick={() => {
+                mutation.mutate({
+                  notes: textareaRef.current.value,
+                });
+              }}
+            >
+              <MagicIcon />
+              Generate
+            </button>
+          </div>
+          {mutation.isPending && (
+            <span className="loading loading-spinner loading-lg mt-10"></span>
+          )}
+          {mutation.isSuccess && <Cards cards={cards} />}
         </div>
-        {mutation.isPending && (
-          <span className="loading loading-spinner loading-lg mt-10"></span>
-        )}
-        {mutation.isSuccess && <Cards cards={cards} />}
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
